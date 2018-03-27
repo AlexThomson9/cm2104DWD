252b0bb1a82927cf86e59c5254c9767ebc04d13d
AlexThomson9 <1507262@rgu.ac.uk>
2018-03-27 11:33:13 +0100

    dfsdf

--- lab15/server.js
+++ lab15/server.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.bodyParser());
 var db;
 MongoClient.connect(url, function(err, database){
  if(err) throw err;
