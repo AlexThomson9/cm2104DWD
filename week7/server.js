4cf3e25e7f87d40440124e2e03545ac5a65c4484
AlexThomson9 <1507262@rgu.ac.uk>
2018-03-27 10:01:04 +0100

    sda

--- week7/server.js
+++ week7/server.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send("hello world! by express");
-
-
-
+app.get('/test', function(req, res){
+ res.send("this is route 2");
 });
 
 app.listen(8080);
