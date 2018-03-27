aa8760a07d81867d8a5a194afdebe4641f36035c
AlexThomson9 <1507262@rgu.ac.uk>
2018-03-27 10:30:41 +0100

    dsa

--- lab15/server.js
+++ lab15/server.js
@@ -2,13 +2,14 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/star_wars_quotes";
 const express = require('express');
 const app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
 var db;
 MongoClient.connect(url, function(err, database){
  if(err) throw err;
  db = database;
  app.listen(8080);
 });
+
 app.get('/all', function(req, res) {
  db.collection('quotes').find().toArray(function(err, result) {
  if (err) throw err;
